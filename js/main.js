6e6c54b2f51fdf5588ced0b6d41bf0ba9a048048
Malissa Moran <malissa.moran@gmail.com>
2014-11-01 19:08:33 -0400

    smaller bg img

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($){
 	
 	// Top fullscreen image with jQuery backstretch
 	// $.backstretch('images/woodward.jpg');
-	$.backstretch('images/bg.jpg');
+	$.backstretch('images/bg.png');
 	
 	// jQuery tooltips
 	$('.teams .social li a').tooltip();
