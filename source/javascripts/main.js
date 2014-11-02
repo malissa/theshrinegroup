4ba0e87f72563cd9a6e1e60f0ea3e7f30f03f07a
Douglas Drouillard <drouillard@gmail.com>
2014-11-02 11:03:12 -0500

    Middleman version of shrinegrp.com

---
+++ source/javascripts/main.js
@@ -0,0 +1,50 @@
+
+jQuery(window).load(function() {
+	
+	"use strict";
+	
+	jQuery('.page-loading').fadeOut(1000, function() {
+		$(this).remove();	
+	});
+	
+});
+
+jQuery(document).ready(function($){
+	
+	"use strict";
+	
+	// Main navigation menu affix function
+	$('.stickem-container').stickem();
+	
+	// jQuery smooth scrolling
+	$('.navbar-collapse ul li a, #header .arrow-link a').bind('click', function(event) {
+		var $anchor = $(this);		
+		$('html, body').stop().animate({
+			scrollTop: parseInt($($anchor.attr('href')).offset().top - 80, 0)
+		}, 1500,'easeInOutExpo');
+		event.preventDefault();
+	});
+	
+	// Top fullscreen image with jQuery backstretch
+	// $.backstretch('images/woodward.jpg');
+	$.backstretch('images/bg2.jpg');
+	
+	// jQuery tooltips
+	$('.teams .social li a').tooltip();
+	
+	$("#contact-form").submit(function() {
+		var $name		= $("#contact-form input[name='name']");
+		var $email		= $("#contact-form input[name='email']");
+		var $website	= $("#contact-form input[name='website']");
+		var $message	= $("#contact-form textarea[name='message']");
+		if($name.val() == "" || $email.val() == "" || $website.val() == "" || $message.val() == "" ){
+			$("#error").html('All fields are required');
+			return false;
+		}
+		return true;
+	});
+	
+	// jQuery placeholder for IE
+	$("input, textarea").placeholder();
+
+});
